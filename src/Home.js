ab959655da48fda9a886d24b11ee57677a9567a3
maheshpatil132 <92781925+maheshpatil132@users.noreply.github.com>
2022-01-19 15:45:11 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import image from './image.svg'
 const Home = () => {
     return (
         <div>
-            <Grid templateColumns={'repeat(auto-fit , minmax(300px , 1fr))'} width={"100%"} minHeight={'90vh'}   px={"9%"}  py={"9%"} gap={"2rem"} >
+            <Grid templateColumns={'repeat(auto-fit , minmax(300px , 1fr))'} width={"100%"} height={'100%'}   px={"9%"}  py={"9%"} gap={"2rem"} >
                 <Box>
                     <Heading mb={"2.5"} fontWeight={"semibold"} textTransform={"capitalize"}>world's largest coding communitiy is here !</Heading>
                     <Text mb={"2.5"} >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsum distinctio doloribus, a dignissimos optio soluta quod alias error aliquam?</Text>
