77979579e25cf5bbf093849d429e95d59d031715
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-07-25 14:01:57 +0530

    updates

--- src/Review.js
+++ src/Review.js
@@ -1,26 +1,22 @@
-import { Avatar, Box, Grid, GridItem, Icon, Text, transform,Flex} from '@chakra-ui/react'
+import {  Grid,  Text, } from '@chakra-ui/react'
 import React from 'react'
 
 import ReviewBox from './ReviewBox'
 const Review = () => {
-
-    const variants = {
-        visible: { opacity: 1 },
-        hidden: { opacity: 0 },
-      }
     return (
         <div>
             <Text textTransform={'capitalize'} textAlign={'center'} fontSize={'5xl'} mb={'3%'} >reviews</Text>
             <Grid templateColumns={'repeat(auto-fit , minmax(300px , 1fr))'} py={5} gap={'2rem'}>
-        
+         
             <ReviewBox />
             <ReviewBox />
             <ReviewBox />
             <ReviewBox />
             <ReviewBox />
             <ReviewBox />
-
+            
             </Grid>
+            <Text textAlign={'center'} textTransform={'capitalize'} fontSize={'4xl'}>made by mahesh patil</Text>
         </div>
     )
 }
